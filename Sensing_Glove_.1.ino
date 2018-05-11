c9cd6f42979b7a14cbdb4d7821de130c4977859f
Ian Davis <idtx314business@gmail.com>
2018-05-11 17:04:21 -0500

    I think I've identified the root issue. In a fit of optimization I apparently commented out the temperature read line. But the reads are sequential so every read after that was reading the wrong register. I'm still seeing some oddities in the roll calculation, possibly signage related? Overall it's looking much healthier though. I'm going to sunset this troubleshooting branch and modify the main tree rather than merge.

--- Sensing_Glove_.1.ino
+++ Sensing_Glove_.1.ino
@@ -119,7 +119,7 @@ void loop(){
   AcX=Wire.read()<<8|Wire.read();                                                           // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)     
   AcY=Wire.read()<<8|Wire.read();                                                           // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
   AcZ=Wire.read()<<8|Wire.read();                                                           // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
-//  Tmp=Wire.read()<<8|Wire.read();                                                           // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
+  Tmp=Wire.read()<<8|Wire.read();                                                           // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
   GyX=Wire.read()<<8|Wire.read();                                                           // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
   GyY=Wire.read()<<8|Wire.read();                                                           // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
   GyZ=Wire.read()<<8|Wire.read();                                                           // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
@@ -163,7 +163,7 @@ void loop(){
 
   //Data Processing
   unsigned long current_time, vibe_timer=0;
-  float time_diff, accel_roll_angle, accel_pitch_angle, A = 1;
+  float time_diff, accel_roll_angle, accel_pitch_angle, A = .1;
   static float gyro_roll_angle=0, gyro_pitch_angle=0, gyro_yaw_angle=0;
   static unsigned long prev_time = 0;
   
